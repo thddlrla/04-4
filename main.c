9278e05717da36bad10b1ee631403b91793c9fef
thddlrla <okjh0999@naver.com>
2023-09-28 17:28:35 +0900

    creat main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[])
+ {
+	
+	unsigned int x;
+	int b;
+	
+	printf("input a number :  ");
+	scanf("%ui", &x);
+	
+	for(b=0; x!=0; x >>=1) 
+	{
+		if(x & 1)
+		{
+			b++;
+		}
+		
+	}
+	printf("The result is : %i\n", b);
+	
+	system("PAUSE");
+	
+	return 0;
+}
